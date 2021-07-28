56a9514fb92649d05543da4d4ddb52fb08a31ccb
ansungho <ansungho22@gmail.com>
2021-07-28 15:29:15 +0900

    node.js 소켓 통신 실전

--- socket_test/app.js
+++ socket_test/app.js
@@ -1,17 +1,25 @@
 const express = require("express");
-const Http = requrire("http");
+const http = require('http')
 const sockeIo = require("socket.io");
 
 
 const app = express();
-const http = Http.createServer(app);
-const io = sockeIo(http,{
+const Http = http.createServer(app);
+
+const io = sockeIo(Http, {
     cors:{
         origin:"*",
         methods:["GET,POST"]
     },
 });
+app.get("/test",(req,res)=>{
+    res.send("익스프레스 잘 커져 있습니다");
+});
 
+
+Http.listen(3000,()=>{
+    console.log("서버가 켜졋습니다");
+});
 io.on("connection",(socket)=>{
     console.log("연결됨");
 
